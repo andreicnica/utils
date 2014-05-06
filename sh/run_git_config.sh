46961f4d095fc5989ad8dbad37e3639e1e548f29
Marcel Gotsch <gts.devel@icloud.com>
2014-05-06 16:02:42 +0200

    Added git-completion.bash, git-prompt.sh, run_git_config.sh and run_vim_config.sh

---
+++ sh/run_git_config.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+usage() {
+  echo "usage: $0 AUTHOR EMAIL"		
+}
+
+set -e
+
+AUTHOR=$1
+EMAIL=$2
+
+if [ -z "$AUTHOR" ] || [ -z "$EMAIL" ]; then
+  usage
+  exit -1
+fi
+
+echo "Using: "
+echo $AUTHOR
+echo $EMAIL
+
+ANSWER=""
+
+while [ "$ANSWER" != "n" ] && [ "$ANSWER" != "Y" ]
+do
+  echo "Proceed? (Y/n)"
+  read ANSWER
+done
+
+if [ "$ANSWER" == "n" ]; then
+  exit 0
+fi
+
+git config --global user.name "$AUTHOR"
+git config --global user.email $EMAIL
+git config --global core.editor vim
+git config --global merge.tool opendiff
+git config --global color.ui true
+
+GIT_COMMIT_TEMPLATE=$HOME/.gitmessage.txt
+echo "[branch]::[ticket: X]\n" > $GIT_COMMIT_TEMPLATE
+echo "what happenend" >> $GIT_COMMIT_TEMPLATE
+
+git config --global commit.template $GIT_COMMIT_TEMPLATE
+
+echo "DONE."
