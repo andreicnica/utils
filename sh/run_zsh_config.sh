5805be99406dee79c2bae7f60a7d2afe6e66ec70
Marcel Gotsch <gts.devel@icloud.com>
2014-10-18 17:53:47 +0200

    * ask function now returns 0 if Y or 1 if n was chosen * improved question blocks if ask "Question"; then ... * Reduced empty lines between the title and the following section * Better information output + continue question * Added custom path for zshrc (optional) * Asks if zsh should be enabled or not

--- sh/run_zsh_config.sh
+++ sh/run_zsh_config.sh
@@ -1,154 +1,165 @@
 #!/bin/sh
 set -e
 
+# CONSTANTS
 PROGNAME=$0
+ZSH_CUSTOM_PATH=$1
+ZSHRC_CUSTOM_PATH=$2
+
+# Default paths
+ZSH_EXECUTABLE=/bin/zsh
+ZSHRC=$HOME/.zshrc
+
+
+# Functions
 
 usage(){
-  echo "usage: $PROGNAME [zsh]"
+    echo "usage: $PROGNAME [zsh] [zshrc]"
 }
 
 ask(){
-  ANSWER=""
+    QUESTION=$1
+    while true; do
+        read -p "$QUESTION? (Y/n)" ANSWER
 
-  while [ "$ANSWER" != "n" ] && [ "$ANSWER" != "Y" ]
-  do
-    read -p "$1 (Y/n)" ANSWER
-  done
-
-  echo "$ANSWER"
+        if [ "$ANSWER" == "Y" ]; then
+            return 0
+        elif [ "$ANSWER" == "n" ]; then
+            return 1
+        fi
+    done
 }
 
-ZSH_EXECUTABLE=""
-ZSHRC=$HOME/.zshrc
-
+# Adds the given input as new line to the zshrc
 add_line(){
-  echo $1 >> $ZSHRC
+    echo $1 >> $ZSHRC
 }
 
+# Adds a new section by inserting a comment from the given input
 add_section(){
-  add_line "\n# $1"
+    add_line "\n# $1"
 }
 
+# Adds a new option to the zshrc
 add_option(){
-  add_line "setopt $1"
+    add_line "setopt $1"
 }
 
+# Adds a new export to the zshrc
 add_export(){
-  add_line "export $1"
+    add_line "export $1"
 }
 
-if [ -z "$1" ]; then
-  ZSH_EXECUTABLE=/bin/bash
-else
-  ZSH_EXECUTABLE=$1
+if [ ! -z "$ZSH_CUSTOM_PATH" ]; then
+    ZSH_EXECUTABLE=$ZSH_CUSTOM_PATH
+fi
+
+if [ ! -z "$ZSHRC_CUSTOM_PATH" ]; then
+    ZSHRC=$ZSHRC_CUSTOM_PATH
 fi
 
 echo "Using zsh @ $ZSH_EXECUTABLE"
+echo "Placing zshrc @ $ZSHRC"
+
+if ! ask "Continue"; then
+    echo "Aborted."
+    exit 0
+fi
 
 if [ ! -s $ZSH_EXECUTABLE ]; then
-  echo "No such file: $ZSH_EXECUTABLE"
-  usage 
-  exit -1
+    echo "No such file: $ZSH_EXECUTABLE"
+    usage 
+    exit -1
 fi
 
 # Check for the existence of a .zshrc file
 if [ -s $ZSHRC ]; then
-  echo "A .zshrc already exists in your home directory!" 
+    echo "A .zshrc already exists"
 
-  ANSWER=$(ask "Do you wish to overwrite it?")
-  
-  if [ "$ANSWER" == "n" ]; then
-    echo "Keeping the old .zshrc file"
-    source $ZSHRC
-    exit 0
-  else
-    rm -f $ZSHRC
-  fi
+    if ask "Do you wish to keep it"; then
+        echo "Keeping the old .zshrc file"
+        source $ZSHRC
+        exit 0
+    else
+        rm -f $ZSHRC
+    fi
 fi
 
 # Create a new .zshrc file
-echo "# Configuration file for you zsh\n" > $ZSHRC
+echo "# Configuration file for you zsh" > $ZSHRC
 
 # Auto cd ?
-ANSWER=$(ask "Enable auto cd?")
-if [ "$ANSWER" == "Y" ]; then
-  add_section "Enabling auto cd"
-  add_option "auto_cd"
+if ask "Enable auto cd"; then
+    add_section "Enabling auto cd"
+    add_option "auto_cd"
 fi
 
 # Colors ?
-ANSWER=$(ask "Enable coloring?")
-if [ "$ANSWER" == "Y" ]; then
-  add_section "Enabling colors"
-  add_line "autoload -U colors && colors"
+if ask "Enable colors"; then
+    add_section "Enabling colors"
+    add_line "autoload -U colors && colors"
 
-  ANSWER=$(ask "Use custom color theme?")
-  if [ "$ANSWER" == "Y" ]; then
-    add_export "LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx"
-  fi
+    if ask "Use custom color theme"; then
+        add_export "LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx"
+    fi
 fi
 
 # Add /usr/local/bin to PATH ?
-ANSWER=$(ask "Add /usr/local/bin to PATH?")
-if [ "$ANSWER" == "Y" ]; then
-  add_section "Adding /usr/local/bin to PATH"
-  add_export "PATH=/usr/local/bin:\$PATH"
+if ask "Add /usr/local/bin to the PATH"; then
+    add_section "Adding /usr/local/bin to PATH"
+    add_export "PATH=/usr/local/bin:\$PATH"
 fi
 
 # Add git completion ?
-ANSWER=$(ask "Enable git tools?")
 GIT_PROMPT_ENABLED=false
 
-if [ "$ANSWER" == "Y" ]; then
-  ZSH_VERSION_MAJOR=$(/bin/zsh --version|awk {'print $2'}|sed -E 's/\.[0-9]+\.[0-9]+//')
+if ask "Enable git tools"; then
+    ZSH_VERSION_MAJOR=$(/bin/zsh --version|awk {'print $2'}|sed -E 's/\.[0-9]+\.[0-9]+//')
 
-  if [ $ZSH_VERSION_MAJOR -lt 4 ]; then
-    echo "Your zsh version does not support git completion by default!"
-    echo "Please take a look at the web for the git-completion.zsh script."
-  else
-    add_section "Enabling git autocomplete"
-    add_line "autoload -U compinit && compinit"
-  fi
-
-  ANSWER=$(ask "Enable git prompt?")
-  if [ "$ANSWER" == "Y" ]; then
-    GIT_PROMPT_SRC=https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-    GIT_PROMPT_FILE=$HOME/.git-prompt.sh
-    if [ ! -s $GIT_PROMPT_FILE ]; then
-      curl $GIT_PROMPT_SRC -o $GIT_PROMPT_FILE
+    if [ $ZSH_VERSION_MAJOR -lt 4 ]; then
+        echo "Your zsh version does not support git completion by default!"
+        echo "Please take a look at the web for the git-completion.zsh script."
+    else
+        add_section "Enabling git autocomplete"
+        add_line "autoload -U compinit && compinit"
     fi
 
-    add_line "source $GIT_PROMPT_FILE"
-    add_option "prompt_subst"
+    if ask "Enable git prompt"; then
+        GIT_PROMPT_SRC=https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+        GIT_PROMPT_FILE=$HOME/.git-prompt.sh
+        if [ ! -s $GIT_PROMPT_FILE ]; then
+            curl $GIT_PROMPT_SRC -o $GIT_PROMPT_FILE
+        fi
 
-    GIT_PROMPT_ENABLED=true
-  fi
+        add_line "source $GIT_PROMPT_FILE"
+        add_option "prompt_subst"
+
+        GIT_PROMPT_ENABLED=true
+    fi
 fi
 
 # Overwrite ps1 with custom version
-ANSWER=$(ask "Custom PS1 ?")
-if [ "$ANSWER" == "Y" ]; then
-  add_section "Using custom prompt"
+if ask "Use custom prompt style"; then
+    add_section "Using custom prompt"
 
-  if [ $GIT_PROMPT_ENABLED ]; then
-    add_export "PS1='%n(%l) %F{red}:%F{white} %d%F{red}\$(__git_ps1)\n%F{red}> %F{white}'"
-  else
-    add_export "PS1='%n(%l) %F{red}:%F{white} %d\n%F{red}> %F{white}'"
-  fi
+    if [ $GIT_PROMPT_ENABLED ]; then
+        add_export "PS1='%n(%l) %F{red}:%F{white} %d%F{red}\$(__git_ps1)\n%F{red}> %F{white}'"
+    else
+        add_export "PS1='%n(%l) %F{red}:%F{white} %d\n%F{red}> %F{white}'"
+    fi
 fi
 
 # Add npm bin directory to current path?
-ANSWER=$(ask "Export npm's current binary dir to PATH?")
-if [ "$ANSWER" == "Y" ]; then
-  add_section "Export npm's current binary dir to PATH"
-  add_export "PATH=\$(npm bin):\$PATH"
+if ask "Export npm's binary dir to the PATH"; then
+    add_section "Export npm's current binary dir to PATH"
+    add_export "PATH=\$(npm bin):\$PATH"
 fi
 
 
-
 # Change the user's default shell to zsh
-chsh -s $ZSH_EXECUTABLE
-
-source $ZSHRC
+if ask "Change to zsh now"; then
+    chsh -s $ZSH_EXECUTABLE
+    source $ZSHRC
+fi
 
 echo "DONE."
